742ca08fa879aa5f72e3affeb1a234bf719884d3
KuroN3ko276 <nghaidang276@gmail.com>
2024-04-15 15:30:43 +0700

    Add AStarPathFinding

--- Assets/_Data/Node/Node.cs
+++ Assets/_Data/Node/Node.cs
@@ -8,20 +8,27 @@ public class Node
 {
     public int x = 0;
     public int y = 0;
-    //public int gCost = 0;
-    //public int hCost = 0;
+    public int gCost = 0;
+    public int hCost = 0;
     //public int fCost = 0;
     public float posX = 0;
     //public int weight = 1;
     public bool occupied = false;
     public bool blockPlaced = false;
     public int nodeId;
-    public Node up;
+	public Node parent;
+	public Node up;
     public Node right;
     public Node down;
     public Node left;
     public NodeObj nodeObj;
     public BlockCtrl blockCtrl;
+
+    public int fCost
+    {
+        get { return gCost + hCost; }
+    }
+                                    
 
     public virtual List<Node> Neighbors()
     {
--- Assets/_Data/PathFinding/AStartPathFinding.cs
+++ Assets/_Data/PathFinding/AStartPathFinding.cs
@@ -1,226 +1,209 @@
-//using System.Collections.Generic;
-//using System.Linq;
-//using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 
-//public class AStar : AbstractPathfinding
-//{
-//	[Header("A* Search")]
-//	public GridManagerCtrl ctrl;
-//	public List<Node> openSet = new List<Node>();
-//	public HashSet<Node> closedSet = new HashSet<Node>();
-//	public List<Node> finalPath = new List<Node>();
-//	public List<NodeStep> cameFromNodes = new List<NodeStep>();
-//	public LineRenderer lineRenderer;
+public class AStar : AbstractPathfinding
+{
+	[Header("A* Search")]
+	public GridManagerCtrl ctrl;
+	public List<Node> openSet = new List<Node>();
+	public HashSet<Node> closedSet = new HashSet<Node>();
+	public List<Node> finalPath = new List<Node>();
+	//public List<NodeStep> cameFromNodes = new List<NodeStep>();
+	public LineRenderer lineRenderer;
 
-//	protected override void LoadComponents()
-//	{
-//		base.LoadComponents();
-//		LoadCtrl();
-//		LoadLineRenderer();
-//	}
+	protected override void LoadComponents()
+	{
+		base.LoadComponents();
+		LoadCtrl();
+		LoadLineRenderer();
+	}
 
-//	protected virtual void LoadCtrl()
-//	{
-//		if (ctrl != null) return;
-//		ctrl = transform.parent.GetComponent<GridManagerCtrl>();
-//		Debug.LogWarning(transform.name + " LoadCtrl", gameObject);
-//	}
+	protected virtual void LoadCtrl()
+	{
+		if (ctrl != null) return;
+		ctrl = transform.parent.GetComponent<GridManagerCtrl>();
+		Debug.LogWarning(transform.name + " LoadCtrl", gameObject);
+	}
 
-//	protected virtual void LoadLineRenderer()
-//	{
-//		if (lineRenderer != null) return;
-//		lineRenderer = transform.GetComponent<LineRenderer>();
-//		Debug.LogWarning(transform.name + " LoadLineRenderer", gameObject);
-//	}
+	protected virtual void LoadLineRenderer()
+	{
+		if (lineRenderer != null) return;
+		lineRenderer = transform.GetComponent<LineRenderer>();
+		Debug.LogWarning(transform.name + " LoadLineRenderer", gameObject);
+	}
 
-//	public override LineRenderer GetLineRenderer()
-//	{
-//		return lineRenderer;
-//	}
+	public override LineRenderer GetLineRenderer()
+	{
+		return lineRenderer;
+	}
 
-//	public override void DataReset()
-//	{
-//		openSet.Clear();
-//		finalPath.Clear();
-//		cameFromNodes.Clear();
-//		closedSet.Clear();
-//	}
+	public override void DataReset()
+	{
+		openSet.Clear();
+		finalPath.Clear();
+		//cameFromNodes.Clear();
+		closedSet.Clear();
+	}
 
-//	public override bool FindPath(BlockCtrl startBlock, BlockCtrl targetBlock)
-//	{
-//		Node startNode = startBlock.blockData.node;
-//		Node targetNode = targetBlock.blockData.node;
+	public override bool FindPath(BlockCtrl startBlock, BlockCtrl targetBlock)
+	{
+		if (startBlock == null || targetBlock == null)
+		{
+			Debug.LogWarning("startBlock or targetBlock is null");
+		}
+		Node startNode = startBlock.blockData.node;
+		Node targetNode = targetBlock.blockData.node;
+		openSet.Add(startNode);
 
-//		openSet.Add(startNode);
-//		cameFromNodes.Add(new NodeStep(startNode, startNode));
+		while (openSet.Count > 0)
+		{
+			Node currentNode = openSet[0];
+			for (int i = 1; i < openSet.Count; i++)
+			{
+				if (openSet[i].fCost < currentNode.fCost || (openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost))
+				{
+					currentNode = openSet[i];
+				}
+			}
 
-//		NodeStep nodeStep;
-//		List<NodeStep> steps;
-//		while (openSet.Count > 0)
-//		{
-//			Node currentNode = openSet.OrderBy(n => n.fCost).First();
+			openSet.Remove(currentNode);
+			closedSet.Add(currentNode);
 
-//			if (currentNode == targetNode)
-//			{
-//				finalPath = BuildFinalPath(startNode, targetNode);
-//				break;
-//			}
+			if (currentNode == targetNode)
+			{
+				this.finalPath = this.BuildFinalPath(startNode, targetNode);
+				break;
+			}
 
-//			openSet.Remove(currentNode);
-//			closedSet.Add(currentNode);
+			foreach (Node neighbor in currentNode.Neighbors())
+			{
+				if (neighbor == null) continue;
+				if (this.closedSet.Contains(neighbor)) continue;
+				if (!this.IsValidPosition(neighbor, targetNode)) continue;
 
-//			foreach (Node neighbor in currentNode.Neighbors())
-//			{
-//				if (closedSet.Contains(neighbor) || !this.IsValidPosition(neighbor, targetNode))
-//					continue;
+				int newCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbor);
 
-//				int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbor);
-//				if (newMovementCostToNeighbour < neighbor.gCost || !openSet.Contains(neighbor))
-//				{
-//					neighbor.gCost = newMovementCostToNeighbour;
-//					neighbor.hCost = GetDistance(neighbor, targetNode);
-//					foreach(NodeStep step in cameFromNodes)
-//					{
-//						if(step.toNode == neighbor) step.toNode = currentNode;
-//					}
+				// Additional cost for changing direction
+				if (currentNode.parent != null && GetDirectionChangeCost(currentNode.parent, currentNode, neighbor) > 0)
+				{
+					newCostToNeighbor += 10; // Additional cost for changing direction, you can adjust this value
+				}
 
-//					if (!openSet.Contains(neighbor))
-//					{
-//						openSet.Add(neighbor);
-//						nodeStep = new NodeStep(neighbor, currentNode);
-//						cameFromNodes.Add(nodeStep);
-//						steps = BuildNodeStepPath(neighbor, startNode);
-//						nodeStep.stepsString = GetStringFromSteps(steps);
-//						nodeStep.directionString = GetDirectionsFromSteps(steps);
-//						nodeStep.changeDirectionCount = CountDirectionFrom2Nodes(neighbor, startNode);
-//						if (nodeStep.changeDirectionCount <= 3)
-//							continue;
-//					}
-//				}
-//			}
-//		}
+				if (!openSet.Contains(neighbor) || newCostToNeighbor < neighbor.gCost)
+				{
+					neighbor.gCost = newCostToNeighbor;
+					neighbor.hCost = GetDistance(neighbor, targetNode);
+					neighbor.parent = currentNode;
 
-//		ShowPath();
+					if (!openSet.Contains(neighbor))
+					{
+						openSet.Add(neighbor);
+					}
+				}
+			}
+		}
+		this.ShowPath();
+		return this.IsPathFound(); // No path found
 
-//		return IsPathFound();
-//	}
+	}
 
-//	private bool IsValidPosition(Node node, Node startNode)
-//	{
-//		if (node == startNode) return true;
+	private bool IsValidPosition(Node node, Node startNode)
+	{
+		if (node == startNode) return true;
 
-//		return !node.occupied;
-//	}
+		return !node.occupied;
+	}
 
-//	protected virtual bool IsPathFound()
-//	{
-//		return finalPath.Count > 0;
-//	}
+	static int GetDirectionChangeCost(Node fromNode, Node toNode, Node neighborNode)
+	{
+		// If any of the nodes is null, return 0 (no change in direction)
+		if (fromNode == null || toNode == null || neighborNode == null)
+		{
+			return 0;
+		}
 
-//	protected virtual List<Node> BuildFinalPath(Node startNode, Node targetNode)
-//	{
-//		List<Node> path = new List<Node>();
-//		Node toNode = targetNode;
+		// Calculate the direction vectors from fromNode to toNode and from toNode to neighborNode
+		int fromX = toNode.x - fromNode.x;
+		int fromY = toNode.y - fromNode.y;
+		int toX = neighborNode.x - toNode.x;
+		int toY = neighborNode.y - toNode.y;
 
-//		while (toNode != startNode)
-//		{
-//			path.Add(toNode);
-//			toNode = GetFromNode(toNode);
-//		}
+		// Calculate the dot product of the two direction vectors
+		int dotProduct = (fromX * toX) + (fromY * toY);
 
-//		path.Add(startNode);
-//		path.Reverse();
+		// If the dot product is negative, there is a change in direction
+		// Return a positive cost to indicate the change in direction
+		if (dotProduct < 0)
+		{
+			return 1; // You can adjust this cost based on your preference
+		}
 
-//		return path;
-//	}
+		// No change in direction
+		return 0;
+	}
 
-//	protected virtual Node GetFromNode(Node toNode)
-//	{
-//		return cameFromNodes.Find(item => item.toNode == toNode).fromNode;
-//	}
+	protected virtual int GetDistance(Node nodeA, Node nodeB)
+	{
+		int distX = Mathf.Abs(nodeA.x - nodeB.x);
+		int distY = Mathf.Abs(nodeA.y - nodeB.y);
 
-//	protected virtual List<NodeStep> BuildNodeStepPath(Node currentNode, Node startNode)
-//	{
-//		List<NodeStep> steps = new List<NodeStep>();
+		if (distX > distY)
+			return 14 * distY + 10 * (distX - distY);
+		else
+			return 14 * distX + 10 * (distY - distX);
+	}
+	//kiểm tra xem có đi được từ Node có bị chiếm không
+	protected virtual bool IsPathFound()
+	{
+		return finalPath.Count > 0;
+	}
 
-//		Node checkNode = currentNode;
-//		for (int i = 0; i < cameFromNodes.Count; i++)
-//		{
-//			NodeStep step = cameFromNodes.Find(item => item.toNode == checkNode);
-//			steps.Add(step);
-//			checkNode = step.fromNode;
-//			if (step.fromNode == startNode) break;
-//		}
+	protected virtual List<Node> BuildFinalPath(Node startNode, Node targetNode)
+	{
+		List<Node> path = new List<Node>();
+		Node currentNode = targetNode;
 
-//		return steps;
-//	}
+		while (currentNode != startNode)
+		{
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		path.Add(startNode);
 
-//	protected virtual void ShowPath()
-//	{
-//		lineRenderer.enabled = true;
-//		List<Vector3> listPoint = new List<Vector3>();
-//		foreach (Node node in finalPath)
-//		{
-//			listPoint.Add(node.nodeObj.transform.position);
-//		}
-//		lineRenderer.positionCount = listPoint.Count;
-//		lineRenderer.SetPositions(listPoint.ToArray());
-//	}
+		path.Reverse();
+		return path;
+	}
 
-//	protected virtual int GetDistance(Node nodeA, Node nodeB)
-//	{
-//		int distX = Mathf.Abs(nodeA.x - nodeB.x);
-//		int distY = Mathf.Abs(nodeA.y - nodeB.y);
+	//Hiển thị đường đi ra màn hình
+	protected virtual void ShowPath()
+	{
+		lineRenderer.enabled = true;
+		List<Vector3> listPoint = new List<Vector3>();
+		foreach (Node node in finalPath)
+		{
+			listPoint.Add(node.nodeObj.transform.position);
+		}
+		lineRenderer.positionCount = listPoint.Count;
+		lineRenderer.SetPositions(listPoint.ToArray());
+	}
 
-//		if (distX > distY)
-//			return 14 * distY + 10 * (distX - distY);
-//		else
-//			return 14 * distX + 10 * (distY - distX);
-//	}
+	protected virtual int CountDirectionFromSteps(List<NodeStep> steps)
+	{
+		NodeDirections lastDirection = NodeDirections.noDirection;
+		NodeDirections currentDirection;
+		int turnCount = 0;
+		foreach (NodeStep nodeStep in steps)
+		{
+			currentDirection = nodeStep.direction;
+			if (currentDirection != lastDirection)
+			{
+				lastDirection = currentDirection;
+				turnCount++;
+			}
+		}
 
-//	protected virtual string GetStringFromSteps(List<NodeStep> steps)
-//	{
-//		string stepsString = "";
-//		foreach (NodeStep nodeStep in steps)
-//		{
-//			stepsString += nodeStep.toNode.Name() + "=>";
-//		}
-//		return stepsString;
-//	}
-
-//	protected virtual string GetDirectionsFromSteps(List<NodeStep> steps)
-//	{
-//		string stepsString = "";
-//		foreach (NodeStep nodeStep in steps)
-//		{
-//			stepsString += nodeStep.direction + "=>";
-//		}
-//		return stepsString;
-//	}
-
-//	protected virtual int CountDirectionFrom2Nodes(Node currentNode, Node startNode)
-//	{
-//		int count = 0;
-//		List<NodeStep> steps = BuildNodeStepPath(currentNode, startNode);
-//		count = CountDirectionFromSteps(steps);
-//		return count;
-//	}
-
-//	protected virtual int CountDirectionFromSteps(List<NodeStep> steps)
-//	{
-//		NodeDirections lastDirection = NodeDirections.noDirection;
-//		NodeDirections currentDirection;
-//		int turnCount = 0;
-//		foreach (NodeStep nodeStep in steps)
-//		{
-//			currentDirection = nodeStep.direction;
-//			if (currentDirection != lastDirection)
-//			{
-//				lastDirection = currentDirection;
-//				turnCount++;
-//			}
-//		}
-
-//		return turnCount;
-//	}
-//}
+		return turnCount;
+	}
+}
